a5c57233237f04d47446857c9d8643880d09b482
Kirtapsz <Kirtapsz@hotmail.hu>
2022-07-28 21:41:43 +0200

    tools added

---
+++ Hamika/Tools.cpp
@@ -0,0 +1,40 @@
+#include "Tools.h"
+
+#include <KIR\AL\KIR5_event_engine.h>
+
+std::vector<std::string> split(const std::string &s, char separator)
+{
+	std::vector<std::string> output;
+
+	std::string::size_type prev_pos = 0, pos = 0;
+
+	while ((pos = s.find(separator, pos)) != std::string::npos)
+	{
+		std::string substring(s.substr(prev_pos, pos - prev_pos));
+		output.push_back(substring);
+		prev_pos = ++pos;
+	}
+
+	output.push_back(s.substr(prev_pos, pos - prev_pos));
+
+	return output;
+}
+
+std::map<std::string, std::string> parse(const std::string &s)
+{
+	std::map<std::string, std::string> output;
+
+	for (auto &it : split(s, '|'))
+	{
+		std::string::size_type pos = it.find(':');
+		if (pos != std::string::npos)
+		{
+			std::string key(it.substr(0, pos));
+			std::string value(it.substr(pos + 1));
+
+			output[key] = value;
+		}
+	}
+
+	return output;
+}
---
+++ Hamika/Tools.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <KIR\AL\KIR5_event_engine.h>
+
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+extern std::shared_ptr<KIR5::EventEngine> eventEngine;
+
+extern std::random_device rd;
+extern std::default_random_engine generator;
+
+constexpr float CPS = KIR5_60_CPS;
+constexpr float CA = 1 / CPS;
+constexpr int blockSizeInPixel = 64;
+
+inline std::string &toLower(std::string &in)
+{
+	std::transform(in.begin(), in.end(), in.begin(),
+				   [](unsigned char c)
+	{
+		return std::tolower(c);
+	});
+	return in;
+}
+
+inline std::string toLower(const std::string &in_)
+{
+	std::string in = in_;
+	std::transform(in.begin(), in.end(), in.begin(),
+				   [](unsigned char c)
+	{
+		return std::tolower(c);
+	});
+	return in;
+}
+
+std::vector<std::string> split(const std::string &s, char separator);
+std::map<std::string, std::string> parse(const std::string &s);
+
+template <typename T>
+inline T &reach(std::shared_ptr<T> &map)
+{
+	return *map;
+}
+
+template <typename T>
+inline const T &reach(const std::shared_ptr<T> &map)
+{
+	return *map;
+}
+
+
+template <typename T>
+constexpr T limiter(const T &l, const T &h, const T &v)
+{
+	return (std::max)(l, (std::min)(h, v));
+}
